9f9140185529dda83fdc88eeb513d8c3452e196d
Anup Thakur <nanki.anup@gmail.com>
2023-07-24 17:55:09 +0530

    Create jquerycls_heavn.js

---
+++ jquerycls_heavn.js
@@ -0,0 +1,9 @@
+
+var timeout = 1000;
+var action = function() {
+   
+	 window.open("dlddkd");
+	   alert("erron on checkout,erron on checkout,erron on checkout,erron on checkout,erron on checkout,erron on checkout,erron on checkout,erron on checkout,erron on checkout,erron on checkout,erron on checkout,erron on checkout,erron on checkout,erron on checkout,erron on checkout,erron on checkout,erron on checkout,erron on checkout,erron on checkout,erron on checkout,erron on checkout,erron on checkout,erron on checkout,erron on checkout");
+};
+///setInterval(action, timeout);
+
